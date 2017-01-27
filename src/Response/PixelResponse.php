8a0d6e60c3aedbb6e520c4ccd6b006f61315938b
Carson Full <carsonfull@gmail.com>
2017-01-27 14:51:52 -0600

    Added PixelResponse

---
+++ src/Response/PixelResponse.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace Gmo\Web\Response;
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * A transparent pixel response
+ */
+class PixelResponse extends Response
+{
+    /**
+     * Base64 encoded transparent pixel
+     */
+    const PIXEL = 'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+
+    public function __construct()
+    {
+        parent::__construct(null);
+
+        $this->content = base64_decode(static::PIXEL);
+        $this->headers->set('Content-Type', 'image/gif');
+    }
+
+    public function setContent($content): void
+    {
+        if ($content !== null) {
+            throw new \LogicException('The content cannot be set on a PixelResponse instance.');
+        }
+    }
+}
