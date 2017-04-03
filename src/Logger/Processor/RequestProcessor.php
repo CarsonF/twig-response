3e17c5250468083d65e4fb734b968727600e6367
Carson Full <carsonfull@gmail.com>
2017-04-03 10:35:23 -0500

    Moved RequestProcessor for Monolog from common libray.

---
+++ src/Logger/Processor/RequestProcessor.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types=1);
+
+namespace Gmo\Web\Logger\Processor;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * A Monolog Processor that adds request info.
+ */
+class RequestProcessor
+{
+    /** @var RequestStack */
+    protected $requestStack;
+
+    /**
+     * Constructor.
+     *
+     * @param RequestStack $requestStack
+     */
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    public function __invoke(array $record): array
+    {
+        if (!$request = $this->requestStack->getCurrentRequest()) {
+            return $record;
+        }
+
+        return $this->modify($record, $request);
+    }
+
+    protected function modify(array $record, Request $request): array
+    {
+        $params = [
+            'method'    => $request->getMethod(),
+            'host'      => $request->getHost(),
+            'path'      => $request->getPathInfo(),
+            'query'     => $request->query->all(),
+            'userAgent' => $request->server->get('HTTP_USER_AGENT'),
+        ];
+        if ($request->isMethod('POST') || $request->isMethod('PUT')) {
+            $params['body'] = $request->request->all();
+        }
+        if ($referer = $request->headers->get('referer')) {
+            $params['referer'] = $referer;
+        }
+
+        $record['extra']['request'] = $params;
+
+        return $record;
+    }
+}
