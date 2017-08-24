a9e3ebbc1804baec928c73ea92b9fe36ba1ef5e3
Carson Full <carsonfull@gmail.com>
2017-08-24 12:14:32 -0500

    Add logic to RoutingSP for locale routes.

--- src/Provider/RoutingServiceProvider.php
+++ src/Provider/RoutingServiceProvider.php
@@ -6,6 +6,7 @@ use Gmo\Web\EventListener as Listener;
 use Gmo\Web\RequestFactory;
 use Gmo\Web\Routing\ControllerCollection;
 use Gmo\Web\Routing\LazyUrlGenerator;
+use Gmo\Web\Routing\LocaleControllerCollection;
 use Gmo\Web\Routing\UrlMatcher;
 use Silex\Application;
 use Silex\Provider\UrlGeneratorServiceProvider;
@@ -17,9 +18,32 @@ class RoutingServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
-        $app['controllers_factory'] = function ($app) {
+        $app['supported_locales'] = $app->share(function ($app) {
+            return [$app['locale']];
+        });
+
+        $app['controllers_factory.default'] = function ($app) {
             return new ControllerCollection($app['route_factory']);
         };
+
+        $app['controllers_factory.locale'] = function ($app) {
+            return new LocaleControllerCollection($app['route_factory'], $app['supported_locales']);
+        };
+
+        // Change to use LocaleControllerCollection if locale routes are enabled.
+        $app['controllers_factory'] = function ($app) {
+            if ($app['routing.locale_routes'] ?? false) {
+                return $app['controllers_factory.locale'];
+            }
+
+            return $app['controllers_factory.default'];
+        };
+
+        // Main controller should not add locale prefixes because
+        // this would force all routes to be locale aware.
+        $app['controllers'] = $app->share(function ($app) {
+            return $app['controllers_factory.default'];
+        });
 
         $app['url_matcher'] = $app->share(function ($app) {
             return new UrlMatcher($app['routes'], $app['request_context']);
